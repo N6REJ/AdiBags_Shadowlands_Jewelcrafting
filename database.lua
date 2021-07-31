3a7358f27b627e274478bbb88c5ffe813a66d0bc
Bear <programming@hallhome.us>
2021-07-31 06:04:47 -0500

    Add files via upload
    initial seed

---
+++ database.lua
@@ -0,0 +1,24 @@
+-- AdiBags_Shadowlands_Crafting - jewels Database
+-- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://github.com/N6REJ/AdiBags_Shadowlands_Crafting
+--
+local ADDON_NAME, ADDON_TABLE, addon = ...
+
+-- Create addon table
+N = { }
+ADDON_TABLE.N = N
+
+-- Label to use
+N["FilterTitle"] = "Jewels"
+
+-- Database of items
+N["database"] = {
+	-- ID,	--Item name
+	173108,	-- Oriblase
+	173109,	-- Angerseye
+	173110,	-- Umbryl
+	173170,	-- Essence of Rebirth
+	173171,	-- Essence of Torment
+	173172,	-- Essence of Servitude
+	173173,	-- Essence of Valor
+}
